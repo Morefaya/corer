63d73ce64f8725730161150f0cf064bfbc63b8ee
Jean-charles CAZAKO <jcazako@e3r10p19.42.fr>
2017-02-13 20:43:50 +0100

    starting norme

--- asm_work/asm_source/include/common.h
+++ asm_work/asm_source/include/common.h
@@ -6,7 +6,7 @@
 /*   By: rabougue <rabougue@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/12 13:36:35 by rabougue          #+#    #+#             */
-/*   Updated: 2017/02/11 21:31:16 by jcazako          ###   ########.fr       */
+/*   Updated: 2017/02/13 20:31:34 by jcazako          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,22 +58,22 @@
 # define BAD_ARGUMENT			14
 # define MALLOC					15
 
-# define LIVE 0x01
-# define LD 0x02
-# define ST 0x03
-# define ADD 0x04
-# define SUB 0x05
-# define AND 0x06
-# define OR 0x07
-# define XOR 0x08
-# define ZJUMP 0x09
-# define LDI 0x0a
-# define STI 0x0b
-# define FORK 0x0c
-# define LLD 0x0d
-# define LLDI 0x0e
-# define LFORK 0x0f
-# define AFF 0x10
+# define LIVE	0x01
+# define LD		0x02
+# define ST		0x03
+# define ADD	0x04
+# define SUB	0x05
+# define AND	0x06
+# define OR		0x07
+# define XOR	0x08
+# define ZJUMP	0x09
+# define LDI	0x0a
+# define STI	0x0b
+# define FORK	0x0c
+# define LLD	0x0d
+# define LLDI	0x0e
+# define LFORK	0x0f
+# define AFF	0x10
 
 
 typedef struct		s_op
@@ -85,7 +85,7 @@ typedef struct		s_op
 	uint16_t		n_cycle;
 	char			*desc_instr;
 	bool			carry;
-	bool			dir_indir;// false = 4, true = 2 
+	bool			dir_indir;/*false = 4, true = 2*/
 }					t_op;
 
 typedef struct		s_header
@@ -106,13 +106,12 @@ typedef struct		s_info
 
 typedef struct	s_label
 {
-	char	*str;
-	int	n_inst;
-}		t_label;
+	char			*str;
+	int				n_inst;
+}					t_label;
 
 typedef struct		s_glob
 {
-	//t_op 			op_table[17];
 	t_list			*label;
 	t_info			*list;
 }					t_glob;
@@ -152,62 +151,97 @@ static t_op	g_op_tab[] =
 /*
  ** parse_s_file.c
  */
-char				ocp_calc(char **tab);
-int8_t				parse_s_file(char *file, t_header *header, t_glob *glob);
-/*
- ** main.c
- */
-void				error(int error);
 
-void				parse_name(int *fd, t_header *header);
-void				parse_comment(int *fd, t_header *header);
-int 				parse_info(t_glob *glob, char *line);
+char	ocp_calc(char **tab);
+int8_t	parse_s_file(char *file, t_header *header, t_glob *glob);
+
+/*
+ ** error.c
+ */
+
+void	error(int error);
+
+/*
+ ** parse_name.c
+ */
+
+void	parse_name(int *fd, t_header *header);
+
+/*
+ ** parse_comment.c
+ */
+
+void	parse_comment(int *fd, t_header *header);
+
+/*
+ ** parse_info.c
+ */
+
+int 	parse_info(t_glob *glob, char *line);
+
 /*
  ** tools.c
  */
-int					skip_blank(char *line);
-int					is_cmt(char *line);
-int     			wordnb(char *str);
+
+int		skip_blank(char *line);
+int		is_cmt(char *line);
+int     wordnb(char *str);
+
 /*
  ** check_label.c
  */
+
 void				parse_instructions(int *fd, t_glob *glob);
+
 /*
  ** op_tab.c
  */
-void				init_op_table(t_op *op_table);
-void				free_op_table(t_op *op_table);
-t_info				*new_info(void);
 
+void	init_op_table(t_op *op_table);
+void	free_op_table(t_op *op_table);
+t_info	*new_info(void);
 
 /*
  ** clear_line.c
  */
-char    			*clear_line(char *str);
 
+char	*clear_line(char *str);
 
 /*
  ** write_param.c
  */
-void			write_param(int fd, t_glob glob);
+
+void	write_param(int fd, t_glob glob);
 
 /*
  ** count_byte_inst.c
  */
-int			count_byte_inst(t_info *info);
+
+int		count_byte_inst(t_info *info);
+
 /*
  ** get_label_val.c
  */
+
 int     get_label_val(t_info *info, t_glob glob, int i);
+
 /*
  ** invert.c
  */
+
 int		invert_2(int i);
 int		invert_4(int i);
-long		invert_8(long i);
+long	invert_8(long i);
 
 /*
  ** write_nb_inst.c
  */
-void		write_nb_inst(t_info *info, int fd);
+
+void	write_nb_inst(t_info *info, int fd);
+
+/*
+ ** free_glob.c
+ */
+
+void	free_glob(t_glob *glob);
 #endif
